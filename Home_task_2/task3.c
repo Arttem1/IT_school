6e1c6d16e63640d9ccdc4e63e76d2d573044c459
Артем <40826485+Arttem1@users.noreply.github.com>
2018-12-25 15:24:45 +0300

    add Home_task_2

---
+++ Home_task_2/task3.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int count = 0;
+	int number = 0;
+	printf("Enter number\n");
+	scanf_s("%d", &number);
+	while (number != 0)
+	{
+		number = number / 10;
+		count++;
+	}
+	printf("Size a number %i", count);
+	return 0;
+}
